ffaddc40f7a2deefd9cda6eccfd44b2b1ff08533
Novik0ffAL <novik0ff@protonmail.com>
2019-04-23 09:31:48 +0300

    add project

---
+++ Lab32/Lab32/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+
+namespace Lab32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fetchXml = 
+            @"<fetch mapping='logical'>
+                <entity name='contact'>
+                <attribute name='fullname'/>
+                <filter type='and'>
+                    <condition attribute='jobtitle' operator='eq' value='Purchasing Assistant'/>
+                </filter>
+                </entity>
+            </fetch>";
+
+ 
+            EntityCollection contactlist = Service.Service.GetOrganization().RetrieveMultiple(new FetchExpression(fetchXml));
+            foreach (var cnt in contactlist.Entities)
+            {
+                Console.WriteLine(cnt.Attributes["fullname"].ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lab32/Lab32/Service/Service.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Net;
+using System.ServiceModel.Description;
+
+namespace Lab32.Service
+{
+    class Service
+    {
+        static Uri ServiceUrl = new Uri("http://crm-train.columbus.ru:5555/CRM2016/XRMServices/2011/Organization.svc");
+        static string User = "Administrator";
+        static string Password = "Pass@word99";
+        public static IOrganizationService GetOrganization()
+        {
+            try
+            {
+                var credentials = new ClientCredentials
+                {
+                    Windows = { ClientCredential = new NetworkCredential(User, Password) }
+                };
+                return new OrganizationServiceProxy(ServiceUrl, null, credentials, null);
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException($@"Organization service fail");
+            }
+        }
+    }
+}
